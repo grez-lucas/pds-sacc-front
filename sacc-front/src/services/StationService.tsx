88977803d69ffadf06b43e1a9e689b0984d0e442
grez-lucas <lucasgrez3010@gmail.com>
2023-11-24 14:15:52 -0300

    feat: GetStations and GetLockersStates fetch requests

---
+++ sacc-front/src/services/StationService.tsx
@@ -0,0 +1,41 @@
+import configData from "./config.json";
+
+export async function GetStations() {
+    // Returns all stations in the database
+  return fetch(`${configData.SACC_URL}/station/`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => 
+       response.json()
+    )
+    .then((data) => {
+        console.log("Success:", data);
+        return data;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+export async function GetLockerStates(stationId : number) {
+    // Returns all locker states for a given station id
+    return fetch(`${configData.SACC_URL}/station/${stationId}/verify_slots/`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((response) => 
+            response.json()
+        )
+        .then((data) => {
+            console.log("Success:", data);
+            return data;
+        })
+        .catch((error) => {
+        console.error("Error:", error);
+    })    
+}
