a276dcf477ef548cb22aa1106134a4e077a892f5
unknown <pssilva1@miuandes.cl>
2023-11-27 19:09:16 -0300

    feat: added reservation logs conection

--- sacc-front/src/App.tsx
+++ sacc-front/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import SideBar from "./components/Sidebar";
 import { useState } from "react";
+import ReservationsLogs from "./components/ReservationsLogs";
 
 function App() {
   const [selectedMenu, setSelectedMenu] = useState<string>("Locker Stations");
@@ -11,7 +12,7 @@ function App() {
       {selectedMenu === "Locker Stations" && <h1>Locker Stations</h1>}
       {selectedMenu === "Locker States" && <h1>Locker States</h1>}
       {selectedMenu === "Dashboards" && <h1>Dashboards</h1>}
-      {selectedMenu === "Reservations" && <h1>Reservations</h1>}
+      {selectedMenu === "Reservations" && <ReservationsLogs id={1} />}
     </div>
   );
 }
--- sacc-front/src/components/ReservationsLogs.tsx
+++ sacc-front/src/components/ReservationsLogs.tsx
@@ -1,72 +1,74 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from "react";
+import { GetReservationLog } from "../services/ReservationService";
 
-function ReservationsLogs() {
-  const JASON = {
-    message: "Reservation state history found",
-    reservation_state_history: [
-      {
-        id: 4,
-        state: "FIN",
-        transition_date: "2023-11-22T19:36:31.368573Z",
-        reservation: 1,
-        manager: null,
-      },
-      {
-        id: 3,
-        state: "LOA",
-        transition_date: "2023-11-22T19:36:11.816621Z",
-        reservation: 1,
-        manager: null,
-      },
-      {
-        id: 2,
-        state: "CON",
-        transition_date: "2023-11-22T19:35:54.964727Z",
-        reservation: 1,
-        manager: null,
-      },
-      {
-        id: 1,
-        state: "RES",
-        transition_date: "2023-11-22T19:35:33.879219Z",
-        reservation: 1,
-        manager: null,
-      },
-    ],
-  };
+interface ReservationLogItem {
+  id: number;
+  state: string;
+  transition_date: string;
+  reservation: string;
+  manager: string;
+}
+
+interface ReservationLog {
+  reservation_state_history: ReservationLogItem[];
+}
+
+function ReservationsLogs({ id }: { id: number }) {
+  const [reservationLog, setReservationLog] = useState<ReservationLog | null>(
+    null
+  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchReservationLog = async () => {
+      try {
+        const data = await GetReservationLog(id);
+        setReservationLog(data);
+      } catch (error) {
+        console.error("Error fetching reservation log:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchReservationLog();
+  }, [id]);
 
   return (
     <main className="w-[83%] h-full bg-white flex flex-col items-center">
       <div className="bg-gray-600 w-full h-12 flex items-center justify-center text-white">
         Reservations
       </div>
-      <div className="bg-gray-600 w-75 h-12 flex items-center justify-center text-white mt-10">
+      <div className="bg-gray-600 w-100 h-12 flex items-center justify-center text-white mt-10">
         Reservation Name
       </div>
-      <div className="d-flex justify-content-center mt-4">
-        <table className="table w-75">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>State</th>
-              <th>Transition Date</th>
-              <th>Reservation</th>
-              <th>Manager</th>
-            </tr>
-          </thead>
-          <tbody>
-            {JASON.reservation_state_history.map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.state}</td>
-                <td>{item.transition_date}</td>
-                <td>{item.reservation}</td>
-                <td>{item.manager}</td>
+      <div className="flex justify-center mt-4">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <table className="w-100 table-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">ID</th>
+                <th className="px-4 py-2">State</th>
+                <th className="px-4 py-2">Transition Date</th>
+                <th className="px-4 py-2">Reservation</th>
+                <th className="px-4 py-2">Manager</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {reservationLog?.reservation_state_history.map((item) => (
+                <tr key={item.id}>
+                  <td className="border px-4 py-2">{item.id}</td>
+                  <td className="border px-4 py-2">{item.state}</td>
+                  <td className="border px-4 py-2">{item.transition_date}</td>
+                  <td className="border px-4 py-2">{item.reservation}</td>
+                  <td className="border px-4 py-2">{item.manager}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </main>
   );
