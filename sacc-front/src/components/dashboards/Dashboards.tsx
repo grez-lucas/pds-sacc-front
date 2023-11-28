5642b896e727a73fd9107c45c13d1dd217ebbbb9
grez-lucas <lucasgrez3010@gmail.com>
2023-11-28 19:46:59 -0300

    feat: loading spinner

--- sacc-front/src/components/dashboards/Dashboards.tsx
+++ sacc-front/src/components/dashboards/Dashboards.tsx
@@ -80,7 +80,9 @@ function Dashboards() {
   return (
     <>
       {isLoading ? (
-        <h1>Loading...</h1>
+                  <div className="flex justify-center items-center h-[80%]">
+                  <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+                </div>
       ) : (
         <main className="w-[83%] h-full flex items-center justify-center ">
           <article className="h-[95%] w-[85%]  flex flex-col gap-4">
