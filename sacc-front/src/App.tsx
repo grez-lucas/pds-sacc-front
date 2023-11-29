c2b0067caf237371cff3d6f12884b4ecd618516c
sebastian bobenrieth <sjbobenrieth@miuandes.cl>
2023-11-28 22:47:26 -0300

    Tabla de locker stations

--- sacc-front/src/App.tsx
+++ sacc-front/src/App.tsx
@@ -3,6 +3,7 @@ import SideBar from "./components/Sidebar";
 import { useState } from "react";
 import ReservationsLogs from "./components/ReservationsLogs";
 import Reservations from "./components/Reservations";
+import LockerStations from "./components/LockerStations";
 
 function App() {
   const [selectedMenu, setSelectedMenu] = useState("Locker Stations");
@@ -11,7 +12,7 @@ function App() {
   return (
     <div className="w-screen h-screen flex">
       <SideBar setSelectedMenu={setSelectedMenu} />
-      {selectedMenu === "Locker Stations" && <h1>Locker Stations</h1>}
+      {selectedMenu === "Locker Stations" && <LockerStations/>}
       {selectedMenu === "Locker States" && <h1>Locker States</h1>}
       {selectedMenu === "Dashboards" && <h1>Dashboards</h1>}
       {selectedMenu === "Reservations" && (
---
+++ sacc-front/src/components/LockerStations.tsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { GetStations } from "../services/StationService";
+
+interface Station {
+    id: number;
+    thing_name: string;
+    shadow_name: string;
+    address: string;
+    roleARN: string;
+    active: string; // Assuming 'active' is a property in your data
+    // Add other properties as needed
+  }
+
+function LockerStations() {
+  const [stations, setStations] = useState<Station[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchStations = async () => {
+      try {
+        const data = await GetStations();
+        console.log(data)
+        console.log("Fetched reservation log:", data);
+        setStations(data);
+      } catch (error) {
+        console.error("Error fetching reservation log:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStations();
+  },);
+
+  return (
+    <main className="w-[83%] h-full bg-white flex flex-col items-center">
+      <div className="bg-gray-600 w-full h-12 flex items-center justify-center text-white">
+        Locker Stations
+      </div>
+      <div className="bg-gray-600 w-500 h-12 flex items-center justify-center text-white mt-10">
+        Manage stations
+      </div>
+      <div className="flex justify-center mt-4">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <table className="w-100 table-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">Adress</th>
+                <th className="px-4 py-2">State</th>
+                <th className="px-4 py-2">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stations?.map((item) => (
+                <tr key={item.id}>
+                  <td className="border px-4 py-2">{item.address}</td>
+                  <td className="border px-4 py-2">{item.active ? 'Active right now' : 'Inactive'}</td>
+                  <td className="border px-4 py-2">Botones y cosas</td>
+
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </main>
+  );
+}
+
+export default LockerStations;
